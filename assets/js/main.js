88d85ea9b1177f08422842daa6b67344c881eb7f
Marco Alexander Fischer <marco.a.fischer@t-online.de>
2018-08-27 20:16:02 +0200

    Added scroll animations + extras

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+    const fullPage = $(document).alton({
+        fullSlideContainer: 'full',
+        singleSlideClass: 'slide',
+        useSlideNumbers: true,
+        slideNumbersBorderColor: '#fff',
+        slideNumbersColor: 'transparent',
+        bodyContainer: 'pageWrapper',
+    });
+
+    $(".ScrollDown").click(() => $(".paginate:nth-of-type(2n)").click());
+});
